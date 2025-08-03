1392957b250595131e55333d97b0a848c1a5cd2e
vraelian <devon_casey@hotmail.com>
2025-08-02 23:05:43 -0700

    Delete js directory

--- js/data/dateConfig.js
+++
@@ -1,7 +0,0 @@
-export const DATE_CONFIG = {
-    START_YEAR: 2140,
-    START_DAY_OF_WEEK: 1,
-    DAYS_IN_MONTH: [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31],
-    MONTH_NAMES: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
-    DAY_NAMES: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-};
